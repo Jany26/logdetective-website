8d757a8b098d4135ec1d068bdba89a43db0a08e3
Jan Matufka <jmatufka@redhat.com>
2026-08-13 17:24:31 +0200

    temporary test test
    Signed-off-by: Jan Matufka <jmatufka@redhat.com>

--- backend/tests/spells.py
+++ backend/tests/spells.py
@@ -1,5 +1,11 @@
 """Lyney would approve this :magic:"""
 
+from src.spells import test_something
+
 
 def sort_by_name(item):
     return item["name"]
+
+
+def test_some_input():
+    assert test_something(5) == 10
